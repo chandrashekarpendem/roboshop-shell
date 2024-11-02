08f4852238541aa45cbfebf7729fcc5cb92d856e
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2024-11-02 16:08:49 +0530

    automation for creating the servers

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,24 @@
 script_location=$(pwd)
+echo "\e[35m installing Nginx\e[0m"
 dnf install nginx -y
-systemctl enable nginx
-systemctl start nginx
+
+echo "\e[35m Remove older default nginx content\e[0m"
 rm -rf /usr/share/nginx/html/*
+
+echo "\e[35m Download frontend content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+echo "\e[35m change to nginx path\e[0m"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zi
+
+echo "\e[35m unzipping frontend html content\e[0m"
+unzip /tmp/frontend.zip
+
+echo "\e[35m copying the configuration file\e[0m"
 cp $script_location/files/roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+echo "\e[35m enabling Nginx\e[0m"
+systemctl enable nginx
+
+echo "\e[35m restarting Nginx\e[0m"
 systemctl restart nginx
