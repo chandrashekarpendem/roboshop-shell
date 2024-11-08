1f24c951351ba5c1c7dfc4b75a8954c2c8b7634f
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2024-11-08 11:33:58 +0530

    indentation  error in common file

--- mongodb.sh
+++ mongodb.sh
@@ -1,21 +1,21 @@
 source common.sh
 
-print_head" copying the repo file "
-cp $script_location/files/mongod.conf /etc/yum.repos.d/mongo.repo
+print_head  " copying the repo file "
+cp $script_location/files/mongod.conf /etc/yum.repos.d/mongo.repo $>>LOG
 status_check
 
-print_head" installing mongodb"
-dnf install mongodb-org -y
+print_head " installing mongodb"
+dnf install mongodb-org -y $>>LOG
 status_check
 
-print_head" changing port to 0.0.0.0"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+print_head " changing port to 0.0.0.0"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf $>>LOG
 status_check
 
-print_head" enabling mongodb"
-systemctl enable mongod
+print_head " enabling mongodb"
+systemctl enable mongod $>>LOG
 status_check
 
-print_head" restart mongodb"
-systemctl restart mongod
+print_head " restart mongodb"
+systemctl restart mongod  $>>LOG
 status_check
